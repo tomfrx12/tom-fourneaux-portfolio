ee1b32cab4166f4f411c34aed58d8ddd52b7a784
Tom-Fourneaux-NWS <tfourneaux@normandiewebschool.fr>
2025-01-21 17:01:09 +0100

    js

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,12 +5,10 @@ var closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-/* Set the width of the side navigation to 250px */
 function openNav() {
     sidenav.classList.add("active");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     sidenav.classList.remove("active");
 }
